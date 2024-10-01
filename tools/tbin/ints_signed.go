e51e118156faf312c3a26cec2874c8fb73308102
BubbleFish <daforsastudia@gmail.com>
2024-10-01 19:51:06 +0300

    feat: Add signed ints for writer/reader (just a wrapper)

---
+++ tools/tbin/ints_signed.go
@@ -0,0 +1,42 @@
+package tbin
+
+import "io"
+
+// Has no tests because it doesn't need them
+// It's simply a wrapper for tbin/ints.go
+
+func WriteInt8(writer io.Writer, in int8) error {
+	return WriteUint8(writer, uint8(in))
+}
+
+func WriteInt16(writer io.Writer, in int16) error {
+	return WriteUint16(writer, uint16(in))
+}
+
+func WriteInt32(writer io.Writer, in int32) error {
+	return WriteUint32(writer, uint32(in))
+}
+
+func WriteInt64(writer io.Writer, in int64) error {
+	return WriteUint64(writer, uint64(in))
+}
+
+func Int8(reader io.Reader) (int8, error) {
+	out, err := Uint8(reader)
+	return int8(out), err
+}
+
+func Int16(reader io.Reader) (int16, error) {
+	out, err := Uint16(reader)
+	return int16(out), err
+}
+
+func Int32(reader io.Reader) (int32, error) {
+	out, err := Uint32(reader)
+	return int32(out), err
+}
+
+func Int64(reader io.Reader) (int64, error) {
+	out, err := Uint64(reader)
+	return int64(out), err
+}
