200aadd313ad68637a75570c32be879c38fbf4a6
BubbleFish <daforsastudia@gmail.com>
2024-10-15 14:17:40 +0300

    feat: Add ensure.MapContainsKey()

---
+++ tests/tools/ensure_test.go
@@ -0,0 +1,27 @@
+package tools_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/bbfh-dev/go-tools/tools/ensure"
+	"gotest.tools/assert"
+)
+
+func TestMapContainsKey(test *testing.T) {
+	previous := ensure.OnFail
+	test.Cleanup(func() { ensure.OnFail = previous })
+
+	var result string
+	ensure.OnFail = func(format string, args ...any) {
+		result = fmt.Sprintf(format, args...)
+	}
+
+	in := map[string]int{"a": 1, "b": 2, "c": 3}
+	ensure.MapContainsKey(in, "d")
+	assert.Equal(test, result, fmt.Sprintf(ensure.MAP_CONTAINS_KEY_FMT, "d", "a, b, c"))
+
+	result = ""
+	ensure.MapContainsKey(in, "a")
+	assert.Equal(test, result, "")
+}
---
+++ tools/ensure/ensure.go
@@ -0,0 +1,26 @@
+package ensure
+
+import (
+	"fmt"
+	"os"
+	"sort"
+	"strings"
+
+	"github.com/bbfh-dev/go-tools/tools/tmap"
+)
+
+const MAP_CONTAINS_KEY_FMT = "Map expected to have key '%v' but only had [%s]"
+
+var OnFail = func(format string, args ...any) {
+	fmt.Println("ASSERT FAILED:\n" + fmt.Sprintf(format, args...))
+	os.Exit(1)
+}
+
+func MapContainsKey[K comparable, V any](in map[K]V, key K) {
+	_, ok := in[key]
+	if !ok {
+		keys := tmap.Flatten(in, func(key K, _ V) string { return fmt.Sprintf("%v", key) })
+		sort.Strings(keys)
+		OnFail(MAP_CONTAINS_KEY_FMT, key, strings.Join(keys, ", "))
+	}
+}
